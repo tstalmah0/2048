dc8a287a909761286946e59a37022e6c58f93099
Tommy Stalmah <tstalmah@purdue.edu>
2023-08-18 14:31:06 -0400

    add game loop as well as redo of some of the moves

--- game.py
+++ game.py
@@ -5,11 +5,18 @@
 # class for playing a game
 
 import numpy as np
+import pygame
 
 class Game:
-    def __init__(self, visual=False, board=None) -> None:
+    def __init__(self,
+                 visual=False,
+                 board=None,
+                 add_nums=True,
+                 print_mode_standard=True) -> None:
         self.visual = visual
         self.nums = np.array([2,2,2,2,2,2,2,2,2,4])
+        self.add_nums = add_nums
+        self.print_mode_standard = print_mode_standard
         if board is not None:
             self.board = board
             self.num_zeros = self.count_zeros()
@@ -28,7 +35,9 @@ class Game:
 
     # function to make a left move on the board
     def left(self) -> None:
-        self.board = self.add_number(self.move(self.board))
+        self.board = self.move(self.board)
+        if self.add_nums:
+            self.board = self.add_number(self.board)
 
     # function to make a right move on the board
     def right(self) -> None:
@@ -41,6 +50,19 @@ class Game:
     # function to make a down move on the board
     def down(self) -> None:
         self.board = self.add_number(np.flip(self.move(np.flip(self.board.T,1)),1).T)
+
+    # function that prints the board to the standard output
+    def print_board(self) -> None:
+        # standard print mode
+        if self.print_mode_standard:
+            print(self.board)
+        # fancy print mode
+        else:
+            for row in self.board:
+                for num in row:
+                    out = "_" if num == 0 else str(int(np.log2(num)))
+                    print(out, end="")
+                print()
 
     # function that takes a 4x4 matrix and returns a
     # coresponding 4x4 matrix where a left move has been
@@ -88,15 +110,62 @@ class Game:
         np.random.shuffle(self.nums)
         board[opens[0]//4,opens[0]%4] = self.nums[0]
         return board
+    
+    # function to work as game loop
+    def game_loop(self):
+        # set up the pygame variables
+        pygame.init()
+        screen = pygame.display.set_mode((500,500))
+
+        # set up local variables
+        running = True
+        move_count = 0
+
+        # print the initial board
+        self.print_board()
+        print("moves:", str(move_count), sep=" ")
+
+        # game loop
+        while running:
+            for event in pygame.event.get():
+                # check for clicking the x on the window
+                if event.type == pygame.QUIT:
+                    print("quit")
+                    running = False
+                # check for key presses
+                elif event.type == pygame.KEYDOWN:
+                    # check for pressing escape
+                    if event.key == pygame.K_ESCAPE:
+                        print("esc")
+                        running = False
+                    # check for pressing left arrow
+                    elif event.key == pygame.K_LEFT:
+                        move_count += 1
+                        self.left()
+                        self.print_board()
+                        print("moves:", str(move_count), sep=" ")
+                    # check for pressing right arrow
+                    elif event.key == pygame.K_RIGHT:
+                        move_count += 1
+                        self.right()
+                        self.print_board()
+                        print("moves:", str(move_count), sep=" ")
+                    # check for pressing up arrow
+                    elif event.key == pygame.K_UP:
+                        move_count += 1
+                        self.up()
+                        self.print_board()
+                        print("moves:", str(move_count), sep=" ")
+                    # check for pressing down arrow
+                    elif event.key == pygame.K_DOWN:
+                        move_count += 1
+                        self.down()
+                        self.print_board()
+                        print("moves:", str(move_count), sep=" ")
 
 
 if __name__ == '__main__':
-    game = Game(board=np.array([[0,2,0,2],
-                                [0,0,0,0],
-                                [0,2,4,0],
-                                [0,0,0,0]]))
-    print(game.board)
-    game.down()
-    print(game.board)
+    game = Game(print_mode_standard=False)
+    game.game_loop()
 
     
