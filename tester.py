3c09fbf7f5fc0d50fe00e43ec0cb8192f0918989
Tommy Stalmah <t.stalmah0@gmail.com>
2023-08-21 10:12:19 -0400

    Added test for game's fill_init function

--- tester.py
+++ tester.py
@@ -2,4 +2,19 @@
 #
 # Started 8/20/23
 #
-# class for testing
+# class for testing game class and assosiated functions
+
+import unittest as ut
+from game import Game
+
+class Test_Game(ut.TestCase):
+
+    # funnction for testing if Game.fill_init() adds 2 nunmbers
+    def test_fill_init(self):
+        game = Game()
+        self.assertEqual(game.num_zeros, 14,
+                         "Game init method should fill exactly " +
+                         "2 spots with non zero numbers.")
+        
+if __name__ == '__main__':
+    ut.main()
